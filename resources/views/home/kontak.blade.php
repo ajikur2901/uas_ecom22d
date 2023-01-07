c69efc559e9895aaa52fa1dba545f14f09615167
RezaUmby <191210105@student.mercubuana-yogya.ac.id>
2023-01-07 21:24:03 +0700

    kontak pak paulus

--- resources/views/home/kontak.blade.php
+++ resources/views/home/kontak.blade.php
@@ -2,7 +2,17 @@
 
 @section('content')
     <div class="content-wrapper container">
-        konten kontak disini
-
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Kontak Pemilik
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body text-black">
+        Hubungi : 085643626150 ( Paulus Endarto )
+      </div>
     </div>
+  </div>
 @endsection
