d8aff19b91ceeb0677bbd25a0b3fa629d69e69b7
Reza Putra Bali <113348361+RezaUmby@users.noreply.github.com>
2023-01-07 21:33:54 +0700

    Update UserController.php belum jadi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,4 +17,76 @@ class UserController extends Controller
 
         return view('user.index', $data);
     }
+
+    public function setting() {
+        $data = array('title' => 'Setting Profil');
+        return view('user.setting', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data = array('title' => 'Form User');
+        return view('user.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
