f3ce5aaffcb4bc37f81902f0d019ee0eee3c1634
uwie <wurihandayanisutarjo@gmail.com>
2023-01-11 13:33:07 +0700

    Update kontak.blade.php

--- resources/views/home/kontak.blade.php
+++ resources/views/home/kontak.blade.php
@@ -6,7 +6,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Kontak Pemilik
+        <h3>Kontak Pemilik</h3>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -18,6 +18,9 @@
       </div>
       <div class="accordion-body text-black">
         Maps : Jl. Laksda Adisucipto Km. 7%2C5 No. 5 Catur Tunggal Depok Sleman Daerah Istimewa Yogyakarta, Ambarukmo, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3840.534191509069!2d110.40255781540021!3d-7.78236003056057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59e820a14a41%3A0xc973ff5312158a00!2sJogja%20Adventure%20Tour!5e0!3m2!1sen!2sid!4v1673418530846!5m2!1sen!2sid" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          
       </div>
     </div>
   </div>
